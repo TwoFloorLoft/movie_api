a531045564f7499cef59f328283b1e7ad12f7330
TwoFloorLoft <joaoluisespiguinha@gmail.com>
2021-10-18 17:26:50 +0200

    Exercise 2.5

--- index.js
+++ index.js
@@ -51,8 +51,6 @@ let topMovies = [
 
 app.use(morgan('common'));
 
-app.use('/documentation.html', express.static('public'));
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -63,9 +61,46 @@ app.get('/', (req, res) => {
   res.send('Welcome to myFlix!');
 });
 
+app.get('/documentation', (req, res) => {
+	res.sendFile('public/documentation.html', { root: __dirname });
+});
+
 app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
+
+app.get('/movies/:movieTitle', (req, res) => {
+  let movie = topMovies.find(m => m.title === req.params.movieTitle)
+  res.json(movie)
+}); 
+
+app.get('/genres/:genre', (req, res) => {
+  res.send('Successful GET request of all major genres in the movie field.');
+}); 
+
+app.get('/directors/:directorName', (req, res) => {
+  res.send('Successful GET request of directors information.');
+}); 
+
+app.post('/register', (req, res) => {
+  res.send('Successful POST to the server a new users information.');
+}); 
+
+app.put('/users/:ID/:infoToUpdate/:newValue', (req, res) => {
+  res.send('Successful PUT to update users username information.');
+}); 
+
+app.post('/users/:ID/favorites/:newFavorite', (req, res) => {
+  res.send('Successful POST to the server for a new favorite movie on a specific users profile.');
+}); 
+
+app.delete('/users/:id/unregister', (req, res) => {
+  res.send('Successful DELETE to the server that removes a user from the database.');
+}); 
+
+app.delete('/users/:ID/favorites/:deleteFavorite', (req, res) => {
+  res.send('Successful DELETE of a movie from an individual users favorite list on the database.');
+}); 
 
 // Error handler
 app.use((err, req, res, next) => {
