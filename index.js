44fbcadfe4fd4907f5cba5c3ac7cf5f855acb568
TwoFloorLoft <joaoluisespiguinha@gmail.com>
2021-11-16 12:54:36 +0100

    3.6

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/documentation.html', (req, res) => {
 
 // Get list of all movies
 
-app.get("/movies", (req, res) => {
+app.get("/movies", passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then(function (movies) {
       res.status(201).json(movies);
