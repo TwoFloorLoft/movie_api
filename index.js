cc02123cac440bea75ff1939a828e0a4415b16d1
TwoFloorLoft <joaoluisespiguinha@gmail.com>
2021-11-25 15:04:43 +0100

    3.8

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', 'http://localhost', 'http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:1234', 'https://joaoandrademyflix.netlify.app/', 'http://localhost:8080', 'http://testsite.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
       return callback(new Error(message), false);
     }
